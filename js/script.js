268ad1fb1b25b8a41a28ca79591a54f484c75731
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-10-18 15:24:21 +0200

    created first Milestone

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// creo e chiamo una funzione che racchiude tutta la mia applicazione
+myApp()
+function myApp() {
+    // catturo button in variabile
+    const btn = document.querySelector("button");
+    // metto il bottone in ascolto di un evento
+    btn.addEventListener("click", generation);
+    // creo la funzione che fa partire la generazione
+    function generation() {
+        // variabile = n quadrati da generare
+        const numSquares = 100
+        // catturo play-ground in variabile
+        const playGround = document.getElementById("play-ground");
+        // svuoto il play-ground come reset 
+        playGround.innerHTML = ""
+        // creo il ciclo per stampare i quadrati
+        for (let i = 0; i < numSquares; i++) {
+            // genero un quadrato tramite funzione che si ripeterà in base al numero totale di quadrati
+            let square = drawSquare(numSquares);
+            // appendo il quadrato nel playground
+            playGround.append(square);
+        }
+
+        // DRAWING FUNCTION
+        function drawSquare(totalSquares) {
+            // creo il quadrato
+            const squareEl = document.createElement("div");
+            // aggiungo al quadrato la classe .square
+            squareEl.classList.add("square");
+            // decido quanto sarà grande il quadrato in base al numero totale di quadrati
+            let squareWidth = Math.sqrt(numSquares);
+            squareEl.style.width = `calc(100% / ${squareWidth})`
+            squareEl.style.height =  squareEl.style.width 
+            // aggiungo l'evento al click che fa cambiare il colore di sfondo del quadrato
+            squareEl.addEventListener("click", function() {
+                squareEl.classList.add("active")
+            })
+            return squareEl
+        }
+
+    }
+
+}
